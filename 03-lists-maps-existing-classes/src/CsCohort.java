48ac0d2f5894b5f051c0b15ecc18bdb7da54aaef
Ayaan Kazerouni <ayaan@vt.edu>
2024-01-13 11:06:43 -0800

    Annotate CsCohort using new Casdoc format

--- 03-lists-maps-existing-classes/src/CsCohort.java
+++ 03-lists-maps-existing-classes/src/CsCohort.java
@@ -1,77 +1,134 @@
 /*?
- * Keyword: public class CsCohort
- * This is the <i>declaration</i> of a class called <code>CsCohort</code>.
- * The <code>public</code> keyword means that this class can be used by other classes.
+ * Type: Keyword
+ * Anchor: public class CsCohort
+ * ID: class-declaration 
+ * ---
+ * This is the *declaration* of a public class called `CsCohort`.
+ * 
+ * +++
+ * 
+ * Type: Internal
+ * Parent: class-declaration
+ * Anchor: public
+ * ---
+ * `public` indicates that this class is visible to and can be used by other classes.
  */
 public class CsCohort {
 
    /*?
-    * Block:3
-    * These are the <i>instance variables</i> of this class.
-    * Each <code>CsCohort</code> object will have its own <code>year</code>, <code>enrolled</code>, and <code>retainedPercent</code>.
-    * <code>retainedPercent</code> is also marked as <code>final</code>, which means that its value cannot be changed after it is initialized.
-    */
+   * Type: Block
+   * Title: Instance variables
+   * ID: instance-vars
+   * Range: 9
+   * ---
+   * These are the _instance variables_ of this class.
+   * Each `CsCohort` object will have its own `year`, `enrolled`, and
+   * `retainedPercent`.
+   */ 
    private int year;
+   /*?
+    * Type: Keyword
+    * Anchor: final 
+    * ---
+    * `final` indicates that this variable's value cannot be changed after it is initialized.
+    */
    private final int enrolled;
    private double retainedPercent;
 
    /*?
-    * Block:1
-    * Constructor
-    * This is the <i>constructor</i> for the <code>CsCohort</code> class.
-    * The constructor's job is to create a new <code>CsCohort</code> object.
+    * Type: Block
+    * Range: 16
+    * Title: Constructor
+    * ---
+    * This is the `constructor` for the `CsCohort` class.
+    * The constructor's job is to create a new `CsCohort` object.
     * Here, the constructor takes in 3 parameters — the values of these parameters are given to the instance variables.
     */
    public CsCohort(int year, int inEnroll, double inPercent) {
-    /*?
-    * Keyword:this
-    * <code>this</code> is the keyword used for the object to refer to itself. So, in this example, <code>this.year</code>
-    * refers to this particular <code>CsCohort</code> object's <code>year</code> instance variable. Notice that some of
-    * constructor parameters have the same name as the instance variables. In this case, <code>this</code> is necessary
-    * to help the compiler distinguish between the two.
-    */
+      /*?
+      * Type:Keyword
+      * Anchor:this
+      * ---
+      * `this` is the keyword used for the object to refer to itself.
+      * So, in this example, `this.year` refers to this particular `CsCohort` object's `year` instance variable.
+      * Notice that some of constructor parameters have the same name as the instance variables.
+      * In this case, `this` is necessary to help the compiler distinguish between the two.
+      * For variables where the instance variable and local variables are not named the same, you don't _need_ to use `this`, but I still recommend it because it makes your code more clear.
+      */
       this.year = year;
       this.enrolled = inEnroll;
       this.retainedPercent = inPercent;
    }
 
    /*?
-    * Block:3
-    * Getter method
-    * This is a <i>getter</i> method or <i>accessor</i> method.
-    * It returns the value of the <code>year</code> instance variable.
-    *
-    * Keyword:public
-    * <code>public</code> indicates that the method is visible to other classes. That is, when other classes have a <code>CsCohort</code> object, they can call this method on that object.
-    *
-    * Keyword:int
-    * <code>int</code> is the return type of this method. It indicates that this method returns an integer value. The return type here is dictated by the data type of the <code>year</code> instance variable.
-    */
+   * Type:Block
+   * Range:67
+   * Title: Instance methods
+   * ---
+   * These are the _behaviours_ that each `CsCohort` can perform, using
+   * its instance variables. We refer to these behaviours as _instance methods_.
+   * 
+   * +++
+   * 
+   * Type:Block
+   * Range:3
+   * Title:Getter method
+   * ---
+   * This is a _getter_ method or _accessor_ method.
+   * It returns the value of the `year` instance variable.
+   * 
+   * +++
+   * 
+   * Type:Keyword
+   * Anchor:public
+   * ---
+   * `public` indicates that the method is visible to other classes. That is, when other classes have a `CsCohort` object, they can call this method on that object.
+   * 
+   * +++
+   *
+   * Type:Keyword
+   * Anchor:int
+   * ---
+   * `int` is the return type of this method. It indicates that this method returns an integer value. The return type here is dictated by the data type of the `year` instance variable.
+   */
    public int getYear() {
       return this.year;
    }
 
    /*?
-    * Block:3
-    * Setter method
-    * This is a <i>setter</i> method or <i>mutator</i> method.
-    * It sets the value of the <code>year</code> instance variable.
-    *
-    * Keyword: int year
-    * Unlike the getter method above, it takes in a parameter, which is the new value of the <code>year</code> instance variable. The type of that parameter must match the type of the instance variable.
-    *
-    * Keyword:void
-    * Since this method's job is only to change the value of the <code>year</code> instance variable, it does not return anything. Hence its return type is declared as <code>void</code>. 
-    */
+   * Type:Block
+   * Range:3
+   * Title:Setter method
+   * ---
+   * This is a _setter_ method or _mutator_ method.
+   * It sets the value of the `year` instance variable.
+   * 
+   * +++
+   * 
+   * Type:Keyword
+   * Anchor: int year
+   * ---
+   * Unlike the getter method above, this setter method takes in a parameter, which is the new value to be given to the `year` instance variable. The type of that parameter needs to match the type of the instance variable.
+   * 
+   * +++
+   *
+   * Type:Keyword
+   * Anchor:void
+   * ---
+   * Since this method's job is only to change the value of the `year` instance variable, it does not return anything. Hence its return type is declared as `void`. 
+   */
    public void setYear(int year) {
       this.year = year;
    }
 
    /*?
-    * Keyword:public int getEnrolled
-    * We only provide a getter method for the <code>enrolled</code> instance variable, but not a setter method.
-    * This means that no other class that modify this <code>CsCohort</code>'s <code>enrolled</code> value, but they can still read it.
-    */
+   * Type:Block
+   * Range:3
+   * Title: public int getEnrolled
+   * ---
+   * We only provide a getter method for the `enrolled` instance variable, but not a setter method.
+   * This means that no other class that modify this `CsCohort`'s `enrolled` value, but they can still read it.
+   */
    public int getEnrolled() {
       return this.enrolled;
    }
@@ -81,11 +138,13 @@ public class CsCohort {
    }
 
    /*?
-    * Keyword:public void setRetainedPercent(double newPercent)
-    * This is a setter method for the <code>retainedPercent</code> instance variable.
-    * We use this setter method to perform some validation on the <code>newPercent</code> parameter, before we
-    * assign its value to the <code>retainedPercent</code> instance variable.
-    */
+   * Type:Block
+   * Range:8
+   * Title: setRetainedPercent(double newPercent)
+   * ---
+   * This is a setter method for the `retainedPercent` instance variable.
+   * We use this setter method to perform some validation on the `newPercent` parameter, before we assign its value to the `retainedPercent` instance variable.
+   */
    public void setRetainedPercent(double newPercent) {
       if (newPercent >= 0 && newPercent <= 1) {
          this.retainedPercent = newPercent;
@@ -96,7 +155,7 @@ public class CsCohort {
    }
 
    public int retained() {
-      return (int) (this.retainedPercent * enrolled);
+      return (int) (this.retainedPercent * this.enrolled);
    }
 }
 
