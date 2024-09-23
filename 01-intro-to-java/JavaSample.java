229867d958dabf6b8d3da1a9a3cdd93728099175
Ayaan Kazerouni <ayaank@calpoly.edu>
2024-09-22 17:15:10 -0700

    Small comment added

--- 01-intro-to-java/JavaSample.java
+++ 01-intro-to-java/JavaSample.java
@@ -2,9 +2,10 @@ import java.awt.*;
 
 public class JavaSample
 {
-   // In Java, everything must be inside a class
+   // In Java, everything must be inside a class (this may no longer be true in Java 23 onward)
    // When the class is run, the method below will run
    public static void main(String[] args) {
       System.out.println("The main method is the \"entry point\" of the program.");
    }
+
 }
